3cc4396798fe5d68b90d670eae3d8a2050aa9689
rksingh <rksingh@DIN56001341.corp.capgemini.com>
2018-04-11 16:01:24 +0530

    Adding Lab 2 of Core Java Manual

---
+++ src/emsv1/EntryConsole.java
@@ -0,0 +1,35 @@
+package emsv1;
+
+public class EntryConsole 
+{
+	    public static void main(String[] args)
+		{
+
+	    	 Date doj  = new Date(11,12,2013); 
+	    	 Employee emp = new Employee("Raj","Ghosh",75000,"a5", doj);
+	    	 emp.displayentry();
+	     	 System.out.println();
+	     	 
+	         doj  = new Date(9,6,2013);
+	    	 emp = new Employee("John","Cena",65000,"a5",doj);	    
+	    	 emp.displayentry();
+	    	 System.out.println();
+	     	 
+	         doj  = new Date(8,5,2011);
+	    	 emp = new Employee("Simran","Setia",85000,"a3",doj);	    
+	    	 emp.displayentry();
+	    	 System.out.println();
+	     	 
+	         doj  = new Date(7,6,2014);
+	    	 emp =  new Employee("Dev","Bose",3000,"a2",doj);
+	    	 emp.displayentry();
+	    	 System.out.println();
+	     	 
+	 	     doj  = new Date(12,12,2016);
+	    	 emp = new Employee("Jimmy","Singh",70000,"b5",doj);
+	    	 emp.displayentry();
+	 		 
+	    emp.displaycount();
+	   }
+	}
+
