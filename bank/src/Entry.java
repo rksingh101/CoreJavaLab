dfb3eb6c7f74a39f0756b6aa8c5acb1486452fc2
rksingh <rksingh@DIN56001341.corp.capgemini.com>
2018-04-24 17:54:20 +0530

    Bank Threadripper v1.0

--- bank/src/Entry.java
+++ bank/src/Entry.java
@@ -1,14 +1,42 @@
 import cg.bank.*;
 public class Entry {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 	
 		Bank bank = new Bank();
 		bank.addAccount();
-        Teller teller = new Teller();
+		Teller teller = new Teller();
         teller.setBank(bank);	
-        teller.performTransfer();
-        teller.getBank().showTotalBalance();
+        
+        Thread teller1 = new TellerThread1(teller);
+        teller1.setName("First Teller Thread");
+		Thread teller2 = new TellerThread1(teller);
+		teller2.setName("Second Teller Thread");
+		
+		teller1.start();
+		teller2.start();
+		
+		teller.getBank().showTotalBalance();
+		System.out.println("End of main()....");
 	}
 }
+	
+	class TellerThread1 extends Thread 
+	{
+		private Teller teller;
+
+		public TellerThread1(Teller teller) {
+			this.teller = teller;
+		}
+
+		
+		public void run() {
+			try {
+				teller.performTransfer();
+			 } catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
--- bank/src/cg/bank/Teller.java
+++ bank/src/cg/bank/Teller.java
@@ -2,25 +2,23 @@ package cg.bank;
 import java.util.Random;
 public class Teller 
 {
-	Bank bank;
-	public void performTransfer()
+	private Bank bank;
+	public void performTransfer() throws InterruptedException
 	{
-		//bank = new Bank();
 		Random rand = new Random();
 		// Generate random integers in range 1 to 5
 		int i = 0;
 		while(true)
 		{
+		//	System.out.println("Exchanged"+i);
         int toAccountNo = rand.nextInt((5 - 1) + 1) + 1;
 		int fromAccountNo = rand.nextInt((5 - 1) + 1) + 1;
-		
-		//System.out.println(toAccountNo+" "+fromAccountNo);
-		double amount = 11000;
+		double amount = 110;
+		//Standard amount as example to be passed around
 		Account toAccount = bank.findAccount(toAccountNo);
         Account fromAccount = bank.findAccount(fromAccountNo);
 		bank.transferAmount(toAccount, fromAccount, amount);
-		//bank.showTotalBalance(toAccount, fromAccount);
-		if(i>=5)
+		if(i>20)
 			break;
 		i++;
 		}
