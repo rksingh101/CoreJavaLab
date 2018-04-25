cb98a74f5c7866da7719417fee94e3ba9855c2a2
rksingh <rksingh@DIN56001341.corp.capgemini.com>
2018-04-25 11:52:45 +0530

    Linked List V0.1

---
+++ DS/src/LinkedList.java
@@ -0,0 +1,17 @@
+
+public class LinkedList {
+	public class Node
+	{
+		int data;
+		//Node next;
+		public boolean add(int data)
+		{
+		  this.data = data; 
+		  return true;
+		}
+		public void display()
+		{
+			System.out.println(data);
+		}
+	}
+}
