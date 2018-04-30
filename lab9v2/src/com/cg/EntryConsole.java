113732beaa23926bea1f6760c04195781ecec11d
rksingh <rksingh@DIN56001341.corp.capgemini.com>
2018-04-30 12:59:58 +0530

    Implementing tree sets to lab9 finding an employee

--- lab9v2/src/com/cg/EntryConsole.java
+++ lab9v2/src/com/cg/EntryConsole.java
@@ -11,22 +11,22 @@ public class EntryConsole
 	    	 Date doj  = new Date(11,12,2013);
 	    	 Employee emp = new Employee("Raj","Ghosh",75000,"a5", doj);
 	    	 repo.addEmployee(emp);
-	    	 System.out.println();
+	    	 //System.out.println();
 	     	 
 	         doj  = new Date(9,6,2013);	   
 	         emp = new Employee("John","Cena",65000,"a5",doj);
 	    	 repo.addEmployee(emp);
-	    	 System.out.println();
+	    	 //System.out.println();
 	     	 
 	         doj  = new Date(8,5,2011);
 	         emp = new Employee("Simran","Setia",85000,"a3",doj);	    
-	    	 System.out.println();
+	    	 //System.out.println();
 	    	 repo.addEmployee(emp);
 	    	 
 	     	 doj  = new Date(7,6,2014);
 	     	 emp = new Employee("Dev","Bose",3000,"a2",doj);
 	    	 repo.addEmployee(emp);
-	    	 System.out.println();
+	    	 //System.out.println();
 	     	 
 	 	     doj  = new Date(12,12,2016);
 	 	     emp = new Employee("Jimmy","Singh",70000,"b5",doj);
